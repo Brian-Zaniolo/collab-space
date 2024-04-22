bcbe222036158764b15bd0622e300c0033554510
amine.benmoussa1994@gmail.com <a.benmoussa@itsrizzoli.it>
2024-04-22 16:23:26 +0200

    colors

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './store/store';    
+import store from './store/store';
+import Card from "./components/card/Card";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <Card />
         </Provider>
     </React.StrictMode>
 );
