8d55f404fd30ef528d56ce7ab730e2659c19dc41
amine.benmoussa1994@gmail.com <a.benmoussa@itsrizzoli.it>
2024-04-24 17:52:35 +0200

    start dev card modal

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <Card />
         </Provider>
     </React.StrictMode>
 );
